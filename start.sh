63d9e1e6cfefe749c063705efe0c7df3d909a510
yuanwm <yuanwm@asiainfo.com>
2016-09-19 09:59:43 +0800

    [fix]set mysql env correctly 	modified:   start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 sed -i 's/REDIS_SERVER_IP/'$REDIS_PORT_6379_TCP_ADDR'/g'  ./config/userQuery.conf 
 sed -i 's/REDIS_SERVER_PORT/'$REDIS_PORT_6379_TCP_PORT'/g'  ./config/userQuery.conf
-sed -i 's/MYSQLSERVER_IP/'MYSQLSERVER_IP'/g'  ./config/userQuery.conf
-sed -i 's/MYSQLSERVER_PORT/'MYSQLSERVER_PORT'/g'  ./config/userQuery.conf
-sed -i 's/MYSQLSERVER_USERNAME/'MYSQLSERVER_USERNAME'/g'  ./config/userQuery.conf
-sed -i 's/MYSQLSERVER_PASSWORD/'MYSQLSERVER_PASSWORD'/g'  ./config/userQuery.conf
-sed -i 's/MYSQLSERVER_DBNAME/'MYSQLSERVER_DBNAME'/g'  ./config/userQuery.conf
+sed -i 's/MYSQLSERVER_IP/'$MYSQLSERVER_IP'/g'  ./config/userQuery.conf
+sed -i 's/MYSQLSERVER_PORT/'$MYSQLSERVER_PORT'/g'  ./config/userQuery.conf
+sed -i 's/MYSQLSERVER_USERNAME/'$MYSQLSERVER_USERNAME'/g'  ./config/userQuery.conf
+sed -i 's/MYSQLSERVER_PASSWORD/'$MYSQLSERVER_PASSWORD'/g'  ./config/userQuery.conf
+sed -i 's/MYSQLSERVER_DBNAME/'$MYSQLSERVER_DBNAME'/g'  ./config/userQuery.conf
 
 sed -i 's/THREAD_NUM/'$API_THREAD_NUM'/g'  ./config/userQuery.conf
 sed -i 's/EXPOSE_PORT/'$API_PORT'/g'  ./config/userQuery.conf 
